e32dc03a1155a1fd0c166428140c8f12caeeb54b
Henrique Eberhardt <hen.eberhardt@gmail.com>
2026-09-16 18:58:46 -0300

    Adicionando as libs

--- src/calc/CalculadoraServerImpl.java
+++ src/calc/CalculadoraServerImpl.java
@@ -1,4 +1,25 @@
 package calc;
 
-public class CalculadoraServerImpl {
+import java.util.Date;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "calc.CalculadoraServer")
+public class CalculadoraServerImpl implements CalculadoraServer {
+
+  public float soma(float num1, float num2) {
+    return num1 + num2;
+  }
+
+  public float subtracao(float num1, float num2) {
+    return num1 - num2;
+  }
+
+  public float multiplicacao(float num1, float num2) {
+    return num1 * num2;
+  }
+
+  public float divisao(float num1, float num2) {
+    return num1 / num2;
+  }
+
 }
